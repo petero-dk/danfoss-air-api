fd2a7064122b110870c71cc64fc24772a16d9d28
petero-dk <2478689+petero-dk@users.noreply.github.com>
2025-09-01 17:46:58 -0700

    faster updates for mode and step

--- src/index.ts
+++ src/index.ts
@@ -251,11 +251,11 @@ export class DanfossAir {
     );
 
     params.push(
-      this.buildParam('operation_mode', 'Operation Mode', '', 1, 0x1412, 'byte', 1, '', 60)
+      this.buildParam('operation_mode', 'Operation Mode', '', 1, 0x1412, 'byte', 1, '')
     );
 
     params.push(
-      this.buildParam('fan_step', 'Fan Step', '', 1, 0x1561, 'byte', 1, '', 60)
+      this.buildParam('fan_step', 'Fan Step', '', 1, 0x1561, 'byte', 1, '')
     );
 
     params.push(
